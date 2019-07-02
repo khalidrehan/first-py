dc4aa78993373ab3de0a43dfd8e71b9237a029ae
Khalid Rehan <krehan@iman-group.com>
2019-07-02 15:19:25 +0300

    .

--- hello.py
+++ hello.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 # hello.py
 import myname
-
+# Small Change :)
 name = myname.get_name()
-=======
-# hello.py
-import myname
-
-name = myname.get_name()
->>>>>>> d698d8fb31e855f8658eff0dae6199309c902579
 print("hello {}".format(name))
